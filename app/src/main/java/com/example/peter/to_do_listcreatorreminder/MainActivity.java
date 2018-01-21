1b4eaaadd02ab08f74fb3250b1d9d1e7476475e5
peter5500 <31715971+peter5500@users.noreply.github.com>
2018-01-20 17:22:03 -0800

    update

--- app/src/main/java/com/example/peter/to_do_listcreatorreminder/MainActivity.java
+++ app/src/main/java/com/example/peter/to_do_listcreatorreminder/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.peter.to_do_listcreatorreminder;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.peter.to_do_listcreatorreminder.models.Todo;
+import com.example.peter.to_do_listcreatorreminder.util.DateUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Fab clicked", Toast.LENGTH_SHORT).show();
+                }
+        });
+
+        setupUI(mockdata());
+    }
+    @NonNull
+    private List<Todo> mockdata() {
+        List<Todo> list = new ArrayList<>();
+        for (int i = 0; i < 1000; ++i) {
+            list.add(new Todo("todo " + i, DateUtils.stringToDate("2015 7 29 0:00")));
+        }
+        return list;
     }
 
     private void setupUI(@NonNull List<Todo> todo){
--- app/src/main/java/com/example/peter/to_do_listcreatorreminder/models/Todo.java
+++ app/src/main/java/com/example/peter/to_do_listcreatorreminder/models/Todo.java
@@ -1,9 +1,49 @@
 package com.example.peter.to_do_listcreatorreminder.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Date;
+
 /**
  * Created by peter on 2018/1/20.
  */
 
-public class Todo {
+public class Todo implements Parcelable{
     public String text;
+
+    public Date remindDate;
+
+    public Todo(String text, Date remindDate) {
+        this.text = text;
+        this.remindDate = remindDate;
+    }
+
+    protected Todo(Parcel in) {
+        text = in.readString();
+        remindDate = new Date(in.readLong());
+    }
+
+    public static final Creator<Todo> CREATOR = new Creator<Todo>() {
+        @Override
+        public Todo createFromParcel(Parcel in) {
+            return new Todo(in);
+        }
+
+        @Override
+        public Todo[] newArray(int size) {
+            return new Todo[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(text);
+        parcel.writeLong(remindDate.getTime());
+    }
 }
